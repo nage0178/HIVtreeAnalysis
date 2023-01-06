a4fe96d4ba4ff82ad17642ca7b3e1e073308fedc
Anna Nagel <aanagel@ucdavis.edu>
2023-01-06 13:24:20 -0800

    Removing unnecessary files

--- analysis/runBayesSS.sh
+++ analysis/runBayesSS.sh
@@ -11,15 +11,6 @@
 # Check convergence
 ./mcmcScripts/checkConvergenceSS.sh
 
-# prepare to run again
-./mcmcScripts/mcmcSSNoConverge.sh
-
-# Run again 
-./reRunMcmcTreeSS.sh &> outReRunMcmcSS 
-
-# Check convergence
-./mcmcScripts/checkConvergenceSSRerun.sh
-
 # Parse/combine
 ./mcmcScripts/mcmcCombineOutSS.sh
 
